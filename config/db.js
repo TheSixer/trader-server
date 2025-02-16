6c48a8b7ce00792716a9e37f9dcef1aefab47df4
L.J.Zhang <longjia3713@163.com>
2025-02-17 01:48:00 +1100

    feat: update db

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
